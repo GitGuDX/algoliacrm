b37c02ef3be1045078673c9ec08d60fc40d46e27
GitGuDX <58787321+GitGuDX@users.noreply.github.com>
2021-04-20 19:07:56 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import algolia from 'algoliasearch';
 import contactRoutes from './routes/contacts.js';
 
 const app = express();
-const client = algolia('FIM60MKDNL', '62002d4f3a380e257335f8e130078701');
+const client = algolia('FIM60MKDNL', 'xxx');
 const index = client.initIndex('XD');
 
 
@@ -18,7 +18,7 @@ app.use(cors());
 app.use('/contacts', contactRoutes);
 
 
-const CONNECTION_URL = 'mongodb+srv://Victor:victor123@cluster0.6y876.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://Victor:xxxxxxxx@cluster0.6y876.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
